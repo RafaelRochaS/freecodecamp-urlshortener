d293217c3a335fa0f81f8e9da3d0f5d812f347ff
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2021-06-01 20:53:08 -0300

    Adding DNS lookup

--- server.ts
+++ server.ts
@@ -1,26 +1,40 @@
-import { Next, Request, Response, createServer } from 'restify';
+import { Next, Request, Response, createServer, plugins } from 'restify';
 import corsMiddleware from 'restify-cors-middleware2';
+import dns from 'dns';
 
 const PORT = process.env['PORT'] || 3000;
+const HOST = process.env['HOST'] || '0.0.0.0';
 
 const cors = corsMiddleware({
   origins: ["*"],
 });
 
-
-const respond = (req: Request, res: Response, next: Next) => {
-  res.send('hello ' + req.params.name);
-  next();
-}
-
 let server = createServer();
 
 server.pre(cors.preflight);
 server.use(cors.actual);
+server.use(plugins.bodyParser({}));
 
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+server.get('/', async (req: Request, res: Response) => {
+  res.json({ status: 'online' });
+});
 
-server.listen(PORT, () => {
-  console.log('%s listening at %s', server.name, server.url);
+server.get('/api/shorturl/:url', async (req: Request, res: Response) => {
+  res.json({ param: req.params.url });
+});
+
+server.post('/api/shorturl/', async (req: Request, res: Response) => {
+  if (req.body['url'] == null) {
+    return res.json({ error: 'no url informed' });
+  }
+  dns.lookup(req.body['url'], (err, addresses) => {
+    if (err != null) {
+      return res.json({ error: 'invalid url' })
+    }
+  });
+  return res.json({ param: req.body });
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`${server.name} listening at ${server.url}`);
 });
