007d28a45c9f2166d349bcb7f111a50f579c0554
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2021-06-03 20:37:53 -0300

    Adding in memory data store

--- server.ts
+++ server.ts
@@ -1,4 +1,10 @@
-import { Next, Request, Response, createServer, plugins } from 'restify';
+import {
+  Next,
+  Request,
+  Response,
+  createServer,
+  plugins,
+} from 'restify';
 import corsMiddleware from 'restify-cors-middleware2';
 import dns from 'dns';
 
@@ -9,29 +15,37 @@ const cors = corsMiddleware({
   origins: ['*'],
 });
 
+const urls = new Map();
+
+let index = 0;
+
 function checkUrlMiddleware(req: Request, res: Response, next: Next): void {
-  if (req.body['url'] === null || req.body['url'] === undefined) {
-    res.status(400);
-    return res.json({ error: 'no url informed' });
+  if (req.method === 'POST') {
+    if (req.body['url'] === null || req.body['url'] === undefined) {
+      res.status(400);
+      return res.json({ error: 'no url informed' });
+    }
   }
   return next();
 }
 
 function validateUrlMiddleware(req: Request, res: Response, next: Next): void {
-  let { url } = req.body;
-  if (url.startsWith('https')) {
-    url = url.substring(8);
-  } else if (url.startsWith('http')) {
-    url = url.substring(7);
-  }
-  dns.lookup(url, (err, addresses) => {
-    console.log(addresses);
-    if (err != null) {
-      res.status(400);
-      return res.json({ error: 'invalid url' });
+  if (req.method === 'POST') {
+    let { url } = req.body;
+    if (url.startsWith('https')) {
+      url = url.substring(8);
+    } else if (url.startsWith('http')) {
+      url = url.substring(7);
     }
-    return next();
-  });
+    dns.lookup(url, (err, addresses) => {
+      console.log(addresses);
+      if (err != null) {
+        res.status(400);
+        return res.json({ error: 'invalid url' });
+      }
+      return next();
+    });
+  }
 }
 
 const server = createServer();
@@ -47,11 +61,15 @@ server.get('/', async (req: Request, res: Response) => {
 });
 
 server.get('/api/shorturl/:url', async (req: Request, res: Response) => {
-  res.json({ param: req.params.url });
+  res.json({ status: 'ok' });
 });
 
 server.post('/api/shorturl/', async (req: Request, res: Response) => {
-  res.json({ param: req.body });
+  index += 1;
+  urls.set(index, req.body['url']);
+  console.log(urls);
+  res.status(201);
+  res.json({ original_url: req.body['url'], short_url: index });
 });
 
 server.listen(PORT, HOST, () => {
